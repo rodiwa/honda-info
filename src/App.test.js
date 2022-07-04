b73126ab8b94ba8bb6d37cdd28d8ce12efac94f6
rodiwa <rodiwa@rodismac.local>
2022-07-05 01:19:36 +0530

    trigger-ci

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/deployed to s3/i);
+  const linkElement = screen.getByText(/docker publish/i);
   expect(linkElement).toBeInTheDocument();
 });
