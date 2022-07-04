52867699845ccf4bea3752a7c5ee122f73f905be
rodiwa <rodiwa@rodismac.local>
2022-07-04 20:21:21 +0530

    test-deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Start Here!
+          Deployed to S3!
         </p>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/start here!/i);
+  const linkElement = screen.getByText(/deployed to s3/i);
   expect(linkElement).toBeInTheDocument();
 });
