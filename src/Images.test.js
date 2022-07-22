65e417e2543a640f9026fdf5f973924439cc4607
Rodiwa <rodiwaa@gmail.com>
2022-07-22 20:41:47 +0530

    fix-tests

--- src/Images.test.js
+++ src/Images.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/deployed to s3/i);
+  const linkElement = screen.getByText(/Images!/i);
   expect(linkElement).toBeInTheDocument();
 });
